48b102b8f028225502878110b930621f33a24140
Mohammod Ali <mohammodali651@gmail.com>
2022-11-10 01:09:47 +0600

    data loaded from server site

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 require('dotenv').config()
@@ -15,7 +15,38 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        const serviceCollection = client.db('cloudKitchen').collection('services');
+        const reviewCollection = client.db('cloudKitchen').collection('reviews')
 
+        app.get('/services', async(req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+
+        app.get('/services/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
+        })
+
+        // Reviews api
+        app.post('/reviews', async(req, res) => {
+            const review = req.body;
+            const result = reviewCollection.insertOne(review)
+            res.send(result)
+        })
+    }
+    finally{
+
+    }
+
+}
+run().catch(err => console.error(err))
 
 
 app.get('/', (req, res) => {
