0c4bc065544dc59be270b4060cdd1d7cc51d02fb
Mohammod Ali <mohammodali651@gmail.com>
2022-11-09 22:16:50 +0600

    mongodb added

--- index.js
+++ index.js
@@ -11,13 +11,10 @@ app.use(express.json())
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.7haskyh.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7haskyh.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+
 
 
 app.get('/', (req, res) => {
